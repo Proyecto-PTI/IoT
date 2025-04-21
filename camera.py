6e7b8d840235acd9efc6ebc003ba9aad41da6b98
Lola <lola.constantin21@gmail.com>
2025-04-21 15:46:04 +0200

    Implement MQTT subscriber and shut fswebcam up

--- camera.py
+++ camera.py
@@ -1,7 +1,30 @@
 import time
 import os
+import paho.mqtt.client as mqtt
+
+def on_subscribe(client, userdata, mid, QoS):
+    print("Broker granted the following QoS:" + str(QoS))
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+def on_connect(client, userdata, flags, reason_code):
+    print("Tried to connect with result code" + str(reason_code))
+    client.subscribe("door_command")
+
+
+
+mqttClient = mqtt.Client()
+mqttClient.on_message = on_message
+mqttClient.on_subscribe = on_subscribe
+mqttClient.on_connect = on_connect
+
+mqttClient.connect("nattech.fib.upc.edu", 40402)
+mqttClient.loop_start()
 
 while True:
-    os.system('fswebcam -r 1920x1080 --save ~/Documents/image.jpeg')
+    os.system('fswebcam -r 1920x1080 --save ~/Documents/image.jpeg -q')
 
     time.sleep(1)
+
+loop_end()
