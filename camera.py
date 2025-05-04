1afb95c5672bd9325f9138c9b5cb6e065b049119
Lola <lola.constantin21@gmail.com>
2025-05-04 09:59:47 +0200

    Add servo code

--- camera.py
+++ camera.py
@@ -1,18 +1,31 @@
 import time
 import os
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
 
 def on_subscribe(client, userdata, mid, QoS):
     print("Broker granted the following QoS:" + str(QoS))
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
+    if (msg.topic == "door_command" and str(msg.payload) == "b'Open'"):
+        p.ChangeDutyCycle(10)
+        print("detected command")
+        time.sleep(10)
+        p.ChangeDutyCycle(6)
 
 def on_connect(client, userdata, flags, reason_code):
     print("Tried to connect with result code" + str(reason_code))
     client.subscribe("door_command")
 
-
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(16, True)
+p = GPIO.PWM(18, 50)
+p.start(0)
+p.ChangeDutyCycle(6)
 
 mqttClient = mqtt.Client()
 mqttClient.on_message = on_message
